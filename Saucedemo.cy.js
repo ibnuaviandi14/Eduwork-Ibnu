bf1a4fa44c494e48aa886e40c6962185b81d6f99
ibnuaviandi14 <113602897+ibnuaviandi14@users.noreply.github.com>
2022-10-10 16:12:12 +0700

    Task Fixtures & Static Data

---
+++ Saucedemo.cy.js
@@ -0,0 +1,35 @@
+/// <reference types="cypress" />
+
+describe('Trying Input',() =>{
+    it("Visit the website", () => {
+        cy.visit('https://www.saucedemo.com/')
+        cy.url().should('include','saucedemo')
+        cy.fixture('login').then(login =>{
+            const nama = login.username
+            const pass = login.password
+        })
+    it("Write the username", () => {
+        cy.get('#user-name').clear()
+        cy.get('#user-name').type(nama)
+    })
+
+    it("Write the password", () => {
+        cy.get('#user-name').clear()
+        cy.get('#user-name').type(pass)
+    })
+
+    it("Click Login", () => {
+        cy.get('#login-button').click()
+    })
+
+    it("Click Menu", () => {
+        cy.get('#react-burger-menu-btn').click
+        cy.url().should('include','/inventory.html')
+    })
+
+    it("Click Abouts", () => {
+        cy.get('#react-burger-menu-btn').click
+        cy.url().should('include','https://saucelabs.com/')
+    })
+    })
+})
